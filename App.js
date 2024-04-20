ecbffe27b0d991d52dbe14768d2ae1f3411c8af7
sunksun <sunksunlapunt@gmail.com>
2024-04-20 21:50:04 +0700

    1.0.0

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Text, View } from 'react-native'
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Splash from './screen/Splash';
+import Herb from './screen/Herb';
+import Item1 from './screen/Item1';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Stack = createNativeStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+    return (
+      <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen
+          name="Splash"
+          component={Splash}
+          options={{title: 'ยินดีต้อนรับ'}}
+        />
+        <Stack.Screen
+          name="Herb"
+          component={Herb}
+          options={{title: 'สมุนไพรท้องถิ่นไทเลย'}}
+        />
+        <Stack.Screen
+          name="Item1"
+          component={Item1}
+          options={{title: 'สมุนไพรท้องถิ่นไทเลย'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+    )
+  }
+
+export default App
---
+++ screen/Splash.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Text, Image, Pressable, StyleSheet, View } from 'react-native'
+
+const Splash = ({navigation}) => {
+    const onPress = () => {
+        navigation.navigate('Herb')
+    }
+    return (
+        <View style={{ height: "100%", alignItems: "center", backgroundColor: "#0CA7FF" }}>
+        <View style={styles.top} >
+          <Image source={require('../img/lru.png')} style={{ width: 120, height: 158 }} />
+        </View>
+        <View style={styles.headerText} >
+          <Text style={{ color: "white", fontSize: 24 }}>สมุนไพรท้องถิ่นไทเลย</Text>
+        </View>
+        <View style={styles.middle} >
+          <Pressable onPress={onPress}>
+            <Image source={require('../img/cover_01.jpg')} style={styles.image390} />
+          </Pressable>
+        </View>
+        <View style={styles.bottom} >
+            <Text style={{ color: "white", fontSize: 18 }}>คณะวิทยาศาสตร์และเทคโนโลยี</Text>
+            <Text style={{ color: "white", fontSize: 16 }}>มหาวิทยาลัยราชภัฏเลย</Text>
+        </View>
+      </View>
+      
+    )
+  }
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'space-between',
+      backgroundColor: '#fff',
+      padding: 20,
+      margin: 10,
+    },
+    top: {
+      padding: 10,
+      flex: 0.35,
+      backgroundColor: '#0CA7FF',
+    },
+    headerText: {
+    flex: 0.15,
+    backgroundColor: '#0CA7FF',
+  },
+    middle: {
+        flex: 0.55,
+        backgroundColor: '#0CA7FF',
+      },
+    bottom: {
+        width: "100%",
+        flex: 0.15,
+        backgroundColor: 'hotpink',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    image390: {
+      width: 390, // ปรับขนาดของรูปภาพตามที่คุณต้องการ
+      height: 250, // ปรับขนาดของรูปภาพตามที่คุณต้องการ
+      borderRadius: 10,
+    },
+    image120: {
+      width: 120, // ปรับขนาดของรูปภาพตามที่คุณต้องการ
+      height: 100, // ปรับขนาดของรูปภาพตามที่คุณต้องการ
+      borderRadius: 10,
+    },
+  });
+export default Splash
